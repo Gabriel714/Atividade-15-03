5eac050ff220c8c93a63e0e938a4b95407c7a7cd
Gabriel714 <rgabrielaraujo@gmail.com>
2019-03-15 15:28:37 -0300

    Primeira linha

---
+++ Atividade 15-03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_15_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoje é sexta");
+        }
+
+    }
+}
